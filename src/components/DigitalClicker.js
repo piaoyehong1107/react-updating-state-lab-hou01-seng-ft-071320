4e9cd0ac477afc5ef7a47e0bc80f4794974f1b1b
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-10-01 23:59:05 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+    state={
+        timesClicked: 0
+    }
+
+    handleClick=()=>{
+    let timesClicked=this.state.timesClicked;
+        this.setState({
+            timesClicked: timesClicked+1
+        })
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={
+                    this.handleClick
+                }>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,49 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component {
+    state={
+        errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: {
+            resolution: '1080p'
+          }
+        }
+      }
+      changeBitrate=()=>{
+        this.setState({
+            settings: {
+              ...this.state.settings,
+              bitrate: 12
+            }
+          });
+      }
+      changeReso=()=>{
+        this.setState({
+            settings: {
+              ...this.state.settings,
+              video: {
+                ...this.state.settings.video,
+                resolution: '720p'
+              }
+            }
+          });
+    }
+    render() {
+        console.log(this.state.settings.bitrate)
+        return (
+            <div>
+              <button className='bitrate' onClick={
+                  this.changeBitrate}>
+                {this.state.settings.bitrate}
+              </button>
+              <button className='resolution' onClick={
+                  this.changeReso}>
+                {this.state.settings.video.resolution}
+              </button>
+            </div>
+        )
+    }
+}
